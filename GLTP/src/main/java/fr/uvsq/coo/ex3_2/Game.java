4f07014ab1fbe8458147e75a6c89496b4e160be4
uvsq21505696 <faladejoannes2@gmail.com>
2015-11-12 13:03:23 +0100

    commit cinq exe3_2

--- GLTP/src/main/java/fr/uvsq/coo/ex3_2/Game.java
+++ GLTP/src/main/java/fr/uvsq/coo/ex3_2/Game.java
@@ -10,37 +10,39 @@ package fr.uvsq.coo.ex3_2;
  * @author pc
  */
 public class Game {
-    private int score = 0;
-    
-    private int rolls[] = new int[21];
-    private int currentRoll = 0;
-    
-    
-    public void roll(int pins) 
-    { 
-        score += pins;
-        rolls[currentRoll++] = pins;
-    }
-    
-    public int score() 
-        {
-            int score = 0;
-            int i = 0;
-            for (int frame = 0; frame < 10; frame++)
-            {
-                if (rolls[i] + rolls[i+1] == 10) // spare
-                {
-                    score += 10+rolls[i+2];
-                    i+=2;
-                }
-                else
-                {
-                    score += rolls[i] + rolls[i + 1];
-                    i += 2;
-                }
-            }
-            
-            
-            return score;
-        }
+private int rolls[] = new int[21];
+private int currentRoll = 0;
+public void roll(int pins) {
+rolls[currentRoll++] = pins;
+}
+public int score() {
+int score = 0;
+int frameIndex = 0;
+for (int frame = 0; frame < 10; frame++) {
+if (rolls[frameIndex] == 10) // strike
+{
+score += 10 + strikeBonus(frameIndex);
+frameIndex++;
+} else if (isSpare(frameIndex)) {
+score += 10 + spareBonus(frameIndex);
+frameIndex += 2;
+} else {
+score += sumOfBallsInFrame(frameIndex);
+frameIndex += 2;
+}
+}
+return score;
+}
+private int sumOfBallsInFrame(int frameIndex) {
+return rolls[frameIndex]+rolls[frameIndex+1];
+}
+private int spareBonus(int frameIndex) {
+return rolls[frameIndex + 2];
+}
+private int strikeBonus(int frameIndex) {
+return rolls[frameIndex+1]+rolls[frameIndex+2];
+}
+private boolean isSpare(int frameIndex) {
+return rolls[frameIndex]+rolls[frameIndex+1] == 10;
+}
 }
--- GLTP/src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
+++ GLTP/src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
@@ -10,37 +10,40 @@ import junit.framework.TestCase;
  * @author pc
  */
 public class BowlingGameTest extends TestCase {
-    
     private Game g;
     
-    protected void setUp() throws Exception{
-        g = new Game();
-    }
-   
-    public void testGutterGame() throws Exception {
-    
-        
-        rollMany(20,0);
-    assertEquals(0, g.score());
+    protected void setUp() throws Exception {
+    g = new Game();
     }
     
     private void rollMany(int n, int pins) {
-        for (int i = 0; i < n; i++)
-        g.roll(pins);
-    }
-    
-    public void testAllOnes() throws Exception {
-      
-       rollMany(20,1);
-        assertEquals(20, g.score());
-    }
-    
-    
-   public void testOneSpare() throws Exception {
-        g.roll(5);
-        g.roll(5); // spare
-        g.roll(3);
-        rollMany(17,0);
-        assertEquals(16,g.score());
+for (int i = 0; i < n; i++)
+g.roll(pins);
+}
+
+    public void testGutterGame() throws Exception {
+rollMany(20, 0);
+assertEquals(0, g.score());
+}
+public void testAllOnes() throws Exception {
+rollMany(20,1);
+assertEquals(20, g.score());
+}
+public void testOneSpare() throws Exception {
+rollSpare();
+g.roll(3);
+rollMany(17,0);
+assertEquals(16,g.score());
+}
+public void testOneStrike() throws Exception {
+g.roll(10); // strike
+g.roll(3);
+g.roll(4);
+rollMany(16, 0);
+assertEquals(24, g.score());
+}
+private void rollSpare() {
+g.roll(5);
+g.roll(5);
 }
 }
