c47f3afd9dbb373e89b3d6350c8ed371382f4bd4
uvsq21505696 <faladejoannes2@gmail.com>
2015-11-12 12:31:27 +0100

    thirdTest_ex3_2

--- GLTP/src/main/java/fr/uvsq/coo/ex3_2/Game.java
+++ GLTP/src/main/java/fr/uvsq/coo/ex3_2/Game.java
@@ -10,15 +10,37 @@ package fr.uvsq.coo.ex3_2;
  * @author pc
  */
 public class Game {
-    
     private int score = 0;
+    
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+    
+    
     public void roll(int pins) 
     { 
         score += pins;
+        rolls[currentRoll++] = pins;
     }
     
     public int score() 
         {
+            int score = 0;
+            int i = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (rolls[i] + rolls[i+1] == 10) // spare
+                {
+                    score += 10+rolls[i+2];
+                    i+=2;
+                }
+                else
+                {
+                    score += rolls[i] + rolls[i + 1];
+                    i += 2;
+                }
+            }
+            
+            
             return score;
         }
 }
