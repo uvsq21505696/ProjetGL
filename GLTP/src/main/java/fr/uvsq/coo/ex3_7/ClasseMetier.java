1433b74e486bdb0f1105324f9cc112cf951558c2
uvsq21505696 <faladejoannes2@gmail.com>
2015-11-19 02:20:38 +0100

    commitexe3_7

---
+++ GLTP/src/main/java/fr/uvsq/coo/ex3_7/ClasseMetier.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.coo.ex3_7;
+
+/**
+ *
+ * @author pc
+ */
+public class ClasseMetier {
+    
+    
+    public void unMetier(InversionMetier inv)
+    {
+        
+    }
+}
---
+++ GLTP/src/main/java/fr/uvsq/coo/ex3_7/Metier.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.coo.ex3_7;
+
+/**
+ *
+ * @author pc
+ */
+public class Metier implements InversionMetier{
+
+    public void unMetier() {
+       
+        System.out.println("debut metier");
+        
+        
+        // code...
+        
+        System.out.println("fin metier");
+    }
+    
+}
